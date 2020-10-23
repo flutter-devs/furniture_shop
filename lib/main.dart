19dc670fc57a1bbaffd5013e98b664b20c86c28f
anubhav11803451 <anubhav.11803451@lpu.in>
2020-10-23 14:48:04 +0530

    Initial commit

---
+++ lib/main.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(FurnitureShop());
+}
+
+class FurnitureShop extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Furniture Shop',
+    );
+  }
+}
